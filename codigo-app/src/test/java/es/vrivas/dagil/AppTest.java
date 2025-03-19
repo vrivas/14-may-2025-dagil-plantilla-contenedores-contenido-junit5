62e65c6992c47b09c54e6f50328f3387af0c74a6
Victor M. Rivas <vrivas@ujaen.es>
2025-03-19 18:04:51 +0100

    Todos los ficheros test con beforeall y afterall

--- codigo-app/src/test/java/es/vrivas/dagil/AppTest.java
+++ codigo-app/src/test/java/es/vrivas/dagil/AppTest.java
@@ -4,15 +4,28 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
 /**
  * Unit test for simple App.
  */
 class AppTest {
+    @BeforeAll
+    public void setUp() {
+        System.out.println("Iniciando test clase App...");
+    }
+
     /**
      * Test de introducción de un conjunto de datos de prueba
      */
     @Test
     public void testIntroducir_datos_de_prueba() {
         assertEquals(6, App.establecer_datos_de_prueba());
+    }
+
+    @AfterAll
+    public void setDown() {
+        System.out.println("Finalizando test clase App...");
     }
 }
