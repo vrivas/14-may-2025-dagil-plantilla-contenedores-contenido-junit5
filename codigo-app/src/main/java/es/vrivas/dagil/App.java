dda3452b239575e7678cc86e2ad051f08996a8f2
Victor M. Rivas Santos <vrivas@ujaen.es>
2025-03-19 11:54:23 +0100

    Main muestra título y autor

--- codigo-app/src/main/java/es/vrivas/dagil/App.java
+++ codigo-app/src/main/java/es/vrivas/dagil/App.java
@@ -1,17 +1,26 @@
 package es.vrivas.dagil;
 
 /**
- * Hello world!
+ * Aplicación para usar las clases Contenedor y Contenido de forma genérica
+ * @author Víctor Rivas <vrivas@ujaen.es>
+ * @date 19-mar-2025
  */
 public final class App {
+    /// Título de la aplicación
+    private static final String TITULO = "Clases Contenedor y Contenido";
+
+    /// Autor/a de la aplicación
+    private static final String AUTOR = "Víctor Rivas <vrivas@ujaen.es>";
+
+    /// Constructor privado para evitar instanciación
     private App() {
     }
 
     /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
+     * Función principal
+     * @param args Argumentos de la línea de comandos
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("\n" + TITULO + "    (por " + AUTOR + ")");
     }
 }
